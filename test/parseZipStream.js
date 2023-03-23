719f827b7202b27d0f272977cf838eb5d4acaecb
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-03-23 17:36:26 +0200

    Got the parser working, now for my changes

--- test/parseZipStream.js
+++ test/parseZipStream.js
@@ -1,14 +1,14 @@
-const asyncZipParser = require("../asyncZipParser");
+const asyncZipParser = require("..");
 const fs = require("fs");
 
-const zip = fs.createReadStream('path/to/archive.zip').pipe(asyncZipParser.ParseZip({forceStream: true}));
-for await (const entry of zip) {
-  const fileName = entry.path;
-  const type = entry.type;
-  const size = entry.vars.uncompressedSize;
-  if (fileName === "this IS the file I'm looking for") {
-    entry.pipe(fs.createWriteStream('output/path'));
-  } else {
-    entry.autodrain();
+const main = async () => {
+  const zip = fs.createReadStream('input.zip').pipe(asyncZipParser.ParseZip({forceStream: true}));
+  for await (const entry of zip) {
+    const fileName = entry.path;
+    const type = entry.type;
+    const size = entry.vars.uncompressedSize;
+    entry.pipe(fs.createWriteStream('output/output.txt'));
   }
 }
+
+main();
