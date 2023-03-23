aa09c633849f38f6dff4892919cd77ba7ad3eaea
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2023-03-23 18:47:04 +0200

    optimized parseZip.js for my use case

--- test/parseZipStream.js
+++ test/parseZipStream.js
@@ -2,12 +2,12 @@ const asyncZipParser = require("..");
 const fs = require("fs");
 
 const main = async () => {
-  const zip = fs.createReadStream('input.zip').pipe(asyncZipParser.ParseZip({forceStream: true}));
+  const zip = fs.createReadStream('input.zip').pipe(asyncZipParser.ParseZip());
   for await (const entry of zip) {
     const fileName = entry.path;
     const type = entry.type;
     const size = entry.vars.uncompressedSize;
-    entry.pipe(fs.createWriteStream('output/output.txt'));
+    entry.pipe(fs.createWriteStream(`output/${fileName}`));
   }
 }
 
