34439866d41a805c44028961cf7919ed2edd734e
Mikael Kolehmainen <mikael.kolehmainen@solidcode.fi>
2023-04-11 15:43:54 +0300

    Added autodrain

--- test/parseZipStream.js
+++ test/parseZipStream.js
@@ -3,11 +3,20 @@ const fs = require("fs");
 
 const main = async () => {
   const zip = fs.createReadStream('input.zip').pipe(asyncZipParser.ParseZip());
+  let i = 0;
+
   for await (const entry of zip) {
-    const fileName = entry.path;
-    const type = entry.type;
-    const size = entry.vars.uncompressedSize;
-    entry.pipe(fs.createWriteStream(`output/${fileName}`));
+    console.log(entry.path);
+    if (entry.path === "test.xlsx") {
+      const fileName = entry.path;
+      const type = entry.type;
+      const size = entry.vars.uncompressedSize;
+      entry.pipe(fs.createWriteStream(`output/${fileName}`));
+    } else {
+      entry.autodrain();
+    }
+    console.log(i);
+    i++;
   }
 }
 
